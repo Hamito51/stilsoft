ddc3182a9006035aa3ad91bdaf1a80b719b69c98
Hamito51 <chuley.plc@gmail.com>
2021-02-04 16:07:58 +0300

    Add pagination

--- store/shop/services.py
+++ store/shop/services.py
@@ -1,12 +1,31 @@
 from django_filters import rest_framework as filters
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from .models import Product
 
 
+
+class PaginationProduct(PageNumberPagination):
+    """Настройки для пагинации"""
+    page_size = 1
+    max_page_size = 200
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'results': data
+        })
+        
+
 class ProductFilter(filters.FilterSet):
-	"""Регистронезависымые фильтры для товаров"""
-	category = filters.CharFilter(field_name='category__name', lookup_expr='iexact')
-	subcategory = filters.CharFilter(field_name='subcategory__name', lookup_expr='iexact')
-	
-	class Meta:
-		model = Product
-		fields = ['category', 'subcategory']
+    """Регистронезависымые фильтры для товаров"""
+    category = filters.CharFilter(field_name='category__name', lookup_expr='iexact')
+    subcategory = filters.CharFilter(field_name='subcategory__name', lookup_expr='iexact')
+    
+    class Meta:
+        model = Product
+        fields = ['category', 'subcategory']
--- store/shop/views.py
+++ store/shop/views.py
@@ -1,15 +1,19 @@
 from django.db import models
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
-from .serializers import ProductListSerializer, ProductDetailSerializer
+from .serializers import ProductListSerializer
+from .serializers import ProductDetailSerializer
 from .models import Product
 from .services import ProductFilter
+from .services import PaginationProduct
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
 	"""Информация о продуктах"""
+	# Настройки для фильтров и пагинации
 	filter_backends = (DjangoFilterBackend,)
 	filterset_class = ProductFilter
+	pagination_class = PaginationProduct
 
 	def get_queryset(self):
 		products = Product.objects.all()
